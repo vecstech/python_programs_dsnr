5b2db14e902d1cc647e6e31ce6518b171c42f6f6
vecstech <vecstraitechsolutions@gmail.com>
2025-11-13 09:50:13 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,5 @@
+print("welcome to texas international academy")
+a=10
+b=25
+result=a+b
+print("addition of",a,b,"is ",result)
---
+++ example2.py
@@ -0,0 +1,6 @@
+user=str(input("please enter username:"))
+pwd=str(input("please enter password:"))
+if(user=="texas" and pwd=="texas123"):
+    print("successfully login  credentials are valid")
+else:
+    print("invalid credentials")
